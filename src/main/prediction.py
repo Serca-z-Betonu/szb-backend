b82bf20ec9fa05bd8d8cecd26e5efae5f81d41db
Kacper Klassa <kacperklassa@gmail.com>
2022-10-09 04:35:51 +0200

    change model

--- src/main/prediction.py
+++ src/main/prediction.py
@@ -12,5 +12,5 @@ class Predictor:
         self._model: Any = tf.keras.models.load_model(PATH_TO_MODEL)
 
     def predict_health_state(self, features: np.ndarray) -> float:
-        preds = self._model.predict(features.reshape(1, -1))
-        return 1 - float(np.argmax(preds))
+        preds = self._model.predict(features.reshape(1, -1)).flatten()
+        return 1 - float(preds[1])
